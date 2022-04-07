fdfaa4fe812a2aa5b30aaa9ca3e74aafa30702b7
Your name <you@example.com>
2022-04-07 12:46:35 -0700

    Printing using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point of the code when running
  * The function (Puts) prints the text and appends the newline character
- * Return: always 0 means success 
+ * Return: always 0 means success
  */
 int main(void)
 {
