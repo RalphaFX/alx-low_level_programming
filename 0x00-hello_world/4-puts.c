b23b4353ab674cb9152b3267d922a89d8fd758a2
Your name <you@example.com>
2022-04-07 12:20:19 -0700

    Print using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - Entry point of the code when running
+ * The function (Puts) prints the text and appends the newline character 
+ * Return: always 0 means success
+ * */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
