886ba284ca1cde77a40cfd6132c46790121d05be
Your name <you@example.com>
2022-04-09 09:41:56 -0700

    Print base10 possible combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,11 +10,11 @@ int main(void)
 	int base10;
 	
 	for (base10 = 0 ; base10 < 10 ; base10++)
-	{	putchar(base10%10 + '0');
-	        if (base10 < 9)
+	{	putchar(base10 % 10 + '0');
+		if (base10 < 9)
 		{	putchar(',');
 		        putchar(' ');
-	        }
+		}
 	}
 	putchar('\n');
 	return (0);
