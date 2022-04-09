b7d49a6790c004491b69909bb0c9a05916b16510
Your name <you@example.com>
2022-04-09 08:11:25 -0700

    Print lower case alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point of the  program
+ * Print lowercase alphabets using putchar
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char lower_case;
+
+	for(lower_case = 'a' ; lower_case <= 'z' ; lower_case++)
+		putchar(lower_case);
+	putchar('\n');
+
+	return (0);
+
+}
