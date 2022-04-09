c3c1bdf8a9a45ecf964c4369c152cf16ddb61605
Your name <you@example.com>
2022-04-09 11:44:34 -0700

    Print possible different combinations for 2 digits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point of the  program
+ * Print all possible combinations of 3 digits
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0 ; a < 100 ; a++)
+	{
+		for (b = 0 ; b < 100 ; b++)
+		{
+			for (c = 50 ; c < 58 ; c++)
+			{
+				if (a < b)
+				{	putchar((a / 10) + 48);
+					putchar((a % 10) + 48);
+					putchar(' ');
+					putchar((b / 10) + 48);
+					putchar((b % 10) + 48);
+
+					if ((a != 98) || (b != 99))
+					{	putchar(',');
+						putchar(' ');
+
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
