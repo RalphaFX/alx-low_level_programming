238f1a26d6f0493604414095037a55c8905467c0
Your name <you@example.com>
2022-04-09 08:28:22 -0700

    Print lower case alphabets except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,10 +10,9 @@ int main(void)
 	char lower_case;
 
 	for (lower_case = 'a' ; lower_case <= 'z' ; lower_case++)
-		if ((lower_case != 'q') && (lower_case != 'e')
+		if ((lower_case != 'q') && (lower_case != 'e'))
 				putchar(lower_case);
 	putchar('\n');
 
 	return (0);
-
 }
