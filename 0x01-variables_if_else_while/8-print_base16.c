bf39b6091c0117ac83a0734e1aa61baf447e4372
Your name <you@example.com>
2022-04-09 09:06:58 -0700

    Print base16 digits and characters in lower case

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point of the  program
+ * Print lowercase alphabets using putchar
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char base16;
+
+	for (base16 = '0' ; base16 <= '9' ; base16++)
+		putchar(base16);
+	for (base16 = 'a' ; base16 <= 'Z' ; base16++)
+		putchar(base16);
+	putchar('\n');
+
+	return (0);
+
+}
