3a4c7a60bb09a2283e72fc57632dba8a0fa04d85
Your name <you@example.com>
2022-04-12 11:57:25 -0700

    Is it a lower case?

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _islower - Entry point
+ * ch: character to be checked
+ * return: 1 lower case
+ * return: 0 Otherwise
+ */
+
+int _islower(int ch)
+{
+	return ((ch >= 97) && (ch <= 122));
+}
