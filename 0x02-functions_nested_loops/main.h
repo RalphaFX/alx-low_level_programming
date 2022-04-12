72640edf2d7aad6dee8fa6123161eb17d7247c12
Your name <you@example.com>
2022-04-12 11:29:06 -0700

    Print alphabets lowercase

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,15 +2,6 @@
 #define MAIN_H
 
 int _putchar(char r);
-void print_alphabet(void)
-{
-	int i;
-
-	for (i = 97 ; i < 123 ; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
-}
+void print_alphabet(void);
 #endif
 /* main_h */
