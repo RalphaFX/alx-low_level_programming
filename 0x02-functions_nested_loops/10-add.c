58b86e091ed4afe75444be01e57400ae5d5471c2
Your name <you@example.com>
2022-04-12 13:30:09 -0700

    Sum of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - adds two integers x and y
+ * @x: first integer
+ * @y: second integer
+ * Return: sum of x and y
+ */
+
+int add(int x, int y)
+{
+	int s;
+
+	s = x + y;
+	return (s);
+}
+
