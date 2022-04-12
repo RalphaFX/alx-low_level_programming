22d95078b13470081ac7076dca2af2e1f99d7849
Your name <you@example.com>
2022-04-12 13:20:19 -0700

    Print multiples of 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * times_table - prints multiples of 9
+ * Return: void
+ *
+ */
+
+void times_table(void)
+{
+	int a = 0;
+	int b;
+	int pro;
+
+	while (a <= 9)
+	{
+		b = 0;
+
+		while (b <= 9)
+		{
+			pro = a * b;
+			if (b == 0)
+			{	_putchar('0' + pro);
+			}
+			else if (pro < 10)
+			{	_putchar(' ');
+				_putchar('0' + pro);
+			}
+			else 
+			{	_putchar('0' + (pro / 10));
+				_putchar('0' + (pro % 10));
+			}
+			if (b < 9)
+			{	_putchar(',');
+				_putchar(' ');
+			}
+			b++;
+		}
+		_putchar('\n');
+		a++;
+	}
+}
