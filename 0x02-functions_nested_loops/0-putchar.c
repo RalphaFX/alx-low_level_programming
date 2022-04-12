78c81fec6ed464d2f8846384513b0b74e0ff75cc
Your name <you@example.com>
2022-04-12 10:05:02 -0700

    Print out _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Print _putchar using the main function
+ * Return: 0 (Success)
+ */
+
+int main(voi)
+{
+	int c;
+	char put[] = "_putchar";
+
+	for (i = 0 ; i < 8 ; i++)
+	{	putchar(put(i));
+	}
+	putchar('\n');
+	return (0);
+}
