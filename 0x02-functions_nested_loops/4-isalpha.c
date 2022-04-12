f3e26e2639e0bbb0c42add515341d103f5ce6a53
Your name <you@example.com>
2022-04-12 12:07:43 -0700

    Is it an alphabet?

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isalpha - Entry Point
+ * Description- Checks whether a character is alphabet or not
+ * @ch - character that is checked
+ * Return: 1 Success , 0 not an alphabet
+ *
+ */
+
+int _isalpha(int ch)
+{
+	if((ch >= 97 && ch <= 122) || (ch >= 65 && ch <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
