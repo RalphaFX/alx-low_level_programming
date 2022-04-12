2c97743fdb4e2fba648aaea34d0276b3a34f62cf
Your name <you@example.com>
2022-04-12 12:32:07 -0700

    Print last Digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _abs - Entry Point
+ * Description: compute absolute value of n
+ * @n: Inteer to be tested
+ * Return: The absolute value of @n
+ *
+ */
+
+int _abs(int n)
+{
+	return (n < 0 ? -1 * n : n);
+}
+/**
+ * print_last_digit: printd the last value of integer n
+ * @n: the Value Integer
+ * Return: The last digit
+ */
+int print_last_digit(int n)
+{
+	int lastD;
+
+	lastD = _abs((n % 10));
+	_putchar(lastD + 48);
+
+	return (lastD);
+}
