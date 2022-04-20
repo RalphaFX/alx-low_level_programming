8015e4e15a5ad4c2a785e7b669687769766359a2
Your name <you@example.com>
2022-04-20 13:16:55 -0700

    Print Half of the string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of the string
+ * @str: string to be halved
+ *
+ */
+
+void puts_half(char *str)
+{
+	int len;
+	int i;
+	int j;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if ((len % 2) == 0)
+	{
+		for (i = (len / 2); str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	} else if ((len % 2))
+	{
+		for (j = ((len - 1) / 2); j < (len - 1); j++)
+		{
+			_putchar(str[j + 1]);
+		}
+	}
+	_putchar('\n');
+}
