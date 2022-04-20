fd0d08e24d26429c6dd2f064b0786d0fc8929a7f
Your name <you@example.com>
2022-04-20 12:10:24 -0700

    Calculate the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of a string
+ * @s: A string to evaluate
+ * Return: The length of the string
+ *
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+
+	while(s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
