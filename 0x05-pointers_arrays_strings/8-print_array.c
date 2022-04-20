6457d4180d60467d2a38473c1ed07aebb2f277ee
Your name <you@example.com>
2022-04-20 13:27:56 -0700

    Print Array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,15 +12,15 @@ void print_array(int *a, int n)
 {
 	int j;
 
-	for (j = 0; j < n j++)
+	for (j = 0; j < n; j++)
 	{
 		if (j == 0)
 		{
-			printf("%d", a[i]);
+			printf("%d", a[j]);
 		}
 		else 
 		{
-			printf(", %d", a[i]);
+			printf(", %d", a[j]);
 		}
 	}
 	printf("\n");
