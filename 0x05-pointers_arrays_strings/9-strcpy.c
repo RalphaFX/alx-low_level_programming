ffa00ce8a9217e96414937141a199bba4dd444b7
Your name <you@example.com>
2022-04-20 13:41:45 -0700

    copy a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *  *_strcpy - copy the string pointed by src including the terminating null byte
+ *
+ * @dest: pointer to which we copy to
+ * @src: string to be copied
+ * Return: dest
+ *
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len;
+	int i;
+
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for ( i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
