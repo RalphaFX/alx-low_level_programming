93ca236cb3d3d96c840c2c617e25ce510e5baa65
Your name <you@example.com>
2022-04-24 04:43:33 -0700

    concatenate two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,34 +1,35 @@
 #include "main.h"
 
 /**
- * *_strcat - concatenates two strings
- * adds the terminating  null byte
- * @dest: pointer to the buffer
- * @src: string to be appended
- * Return: The pointer to the resulting string
+ * *_strcat - concatenates two strings dest and src
+ * appends src to dest
+ * Adds the terminatin null byte
+ * @dest: pointer to the buffer in which we copy
+ * @src: string to be copied
  *
+ * Return: The pointer to the resulting string dest
  */
 char *_strcat(char *dest, char *src)
 {
-	int len;
-	int len2;
-	int len3;
-	int i, j;
+	int len, len2, len3, i, j;
 
 	len = len2 = j = 0;
 
 	while (src[len] != '\0')
+	{
 		len++;
+	}
 
 	while (dest[len2] != '\0')
+	{
 		len2++;
-
-	len3 = (len + len2);
+	}
+	len3 = len + len2;
 
 	for (i = len2; i < len3; i++)
 	{
 		dest[i] = src[j];
-		j++;;
+		j++;
 	}
 	dest[i] = '\0';
 
