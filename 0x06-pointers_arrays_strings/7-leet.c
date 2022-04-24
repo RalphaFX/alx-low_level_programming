d919caa8fe7b9fd313084e9a8e8f24f95674835c
Your name <you@example.com>
2022-04-24 08:17:50 -0700

    Encode a string to 1337 coding

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include  "main.h"
+
+/**
+ * *let - encodes a string to 1337 coding
+ * @s: string to be encoded
+ *
+ * Return: the encoded string
+ */
+char *leet(char *s)
+{
+	int i, j;
+
+	char *characters = "aAeEoOtTlL";
+	char *numbers = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (s[i] == characters[j])
+			{
+				s[i] = numbers[j];
+			}
+		}
+	}
+
+	return (s);
+}
