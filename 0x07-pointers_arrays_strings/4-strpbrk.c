9dc57f6bacadedb7d661ac49168b7cfe66c4b5b3
Your name <you@example.com>
2022-04-26 11:03:58 -0700

    search string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strpbrk - searching a string for a set bytes
+ * @s: string to be scanned
+ * @accept: set of bytes with characters to match
+ *
+ * Return: Pointer to the byte in s that matches one of 
+ * the bytes in accept else return NULL
+ */
+char *strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (&s[i]);
+		}
+	}
+	return (NULL);
+}
