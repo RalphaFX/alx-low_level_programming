b35e4145797e639aeb3c1b129c1d1f598eaef2d5
Your name <you@example.com>
2022-04-26 10:07:16 -0700

    Set memory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_memset - fills the first n bytes of s
+ * @s: starting adress of memory to be filled
+ * @b: desired constant byte
+ * @n: number of bytes to be filled
+ *
+ * Return: pointer to the memory area
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	i = 0;
+
+	while (n > 0)
+	{
+		s[i] = b;
+		i++;
+		n--;
+	}
+	return (s);
+}
