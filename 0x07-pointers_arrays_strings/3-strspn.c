1f804bb3ad11b413ca846491375da0e06445e332
Your name <you@example.com>
2022-04-26 10:53:24 -0700

    Find the length of prefix substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,16 +14,15 @@ unsigned int _strspn(char *s, char *accept)
 
 	len = 0;
 
-	for (i = 0; (s[i] != '\0'); i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; (accept[j] != '\0' && accept[j] != s[i]); j++)
+		for (j = 0; accept[j] != '\0' && accept[j] != s[i]; j++)
 		{
 			if (s[i] == accept[j])
 				len++;
+			if (accept[j] == '\0')
+				return (len);
 		}
-		if (accept[j] == '\0')
-			return (len);
-		
 	}
 	return (len);
 }
