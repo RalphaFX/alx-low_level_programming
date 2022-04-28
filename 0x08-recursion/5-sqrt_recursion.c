7d6f7d87a2914ae4328a6ca1f52dd4b322e7012c
Your name <you@example.com>
2022-04-28 13:44:18 -0700

    Calculate square root of integer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * fndSQ - calculate square root recursively using binary method
+ * @begin: lower value of range
+ * @end: Upper value of range
+ * @v: Number to find the SQ of
+ *
+ * Return: Squareroot of number or -1 if not found
+ */
+int fndSQ(int begin, int end, int v)
+{
+	int center;
+
+	if (end >= begin)
+	{
+		center = begin + ((end - begin) / 2);
+		if (center * center == v)
+			return (center);
+		if (center * center > v)
+			return (fndSQ(begin, center - 1, v));
+		if (center * center < v)
+			return (fndSQ(center + 1, end, v));
+	}
+	return (-1);
+}
+/**
+ * _sqrt_recursion - Entry of the program
+ * Finds the natural square root of an integer
+ * @n: value given
+ * Return: Square root of n or -1
+ *
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (fndSQ(2, n, n));
+}
