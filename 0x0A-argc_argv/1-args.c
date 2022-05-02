ee56cc75b87a597c41067c6e9bd4382f2d3bf799
Your name <you@example.com>
2022-05-02 15:42:53 -0700

    Print number  of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Program prints the number of arguments passed unto it
+ * @argc: Count of arguments
+ * @argv: arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
