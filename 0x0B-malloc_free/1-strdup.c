fe669bf6bb396a9458bf5e2290ed278c8128e743
Your name <you@example.com>
2022-05-04 12:29:34 -0700

    Allocate Memory of a string of characters

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strlen - length of a string
+ * @s: string
+ *
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i++;
+	return (i);
+}
+/**
+ * _strdup - return pointer to allocated memory
+ * @str: string to allocate memory
+ *
+ * Return: pointer to duplicated string => success
+ * NULL => failure
+ */
+char *_strdup(char *str)
+{
+	int j, len;
+	char *p;
+
+	if (str == NULL)
+		return (NULL);
+	len = _strlen(str);
+
+	if (len <= 0)
+		return (NULL);
+	p = malloc(sizeof(char) * len);
+
+	if (p == NULL)
+		free(p);
+		return (NULL);
+	else
+	{
+		for (j=0; j < len; j++)
+			p[j] = str[j];
+		p[j] = '\0';
+		return ((char *)p);
+	}
+}
