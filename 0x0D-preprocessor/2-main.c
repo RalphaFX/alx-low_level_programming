06ad4c4a164169fd666cf570e045cab2a72d6d32
Your name <you@example.com>
2022-05-09 11:32:37 -0700

    Print  file name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry Point
  * prints the file name
- * Return: 0 => Success
+ *Return: 0 => Success
  */
 int main(void)
 {
-	printf("%s\n", _FILE_);
+	printf("%s\n", __FILE__);
 	return (0);
 }
