2b2f7c064b7e31036d401a04b6fbc7a6d13ba9fe
Your name <you@example.com>
2022-05-09 12:17:00 -0700

    defining a struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure definition
+ * typedef: dog_t
+ * @name: char
+ * @age:  int
+ * @owner:  char
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+#endif
