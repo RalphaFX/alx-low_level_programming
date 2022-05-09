c8d4c37c3c46cf5b0f8d2eec25d7314b80f0b8ab
Your name <you@example.com>
2022-05-09 12:45:30 -0700

    Print dog details

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * print_dog -  prints dog 's data 
+ * @d: dog struct type
+ */
+void print_dog(struct dog  *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\nAge: %f\nOwner: %s\n",
+				(*d).name != NULL ?  d->name : "(nil)",
+				(*d).age,
+				(*d).owner != NULL ? d->owner :  "(nil)");
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * struct dog - structure definition
  * typedef: dog_t
@@ -14,9 +15,5 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *onwer);
-void free_dog(dog_t *d);
-
+void
 #endif
