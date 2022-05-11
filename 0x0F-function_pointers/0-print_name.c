267319c00b9831ab341e1328ede340d97ce8859b
Your name <you@example.com>
2022-05-11 11:33:33 -0700

    Print out a name using function pointer method

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints the name
+ * @name: string of characters
+ * @f: function pointer to a function that has arguments
+ * as char and returns nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+	{
+		f(name);
+	}
+}
