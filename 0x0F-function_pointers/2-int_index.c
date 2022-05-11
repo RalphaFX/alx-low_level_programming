ad7cc729ef10bb12e51f386a504484ec90fb8f35
Your name <you@example.com>
2022-05-11 12:40:50 -0700

    Functions Pointer in action with array

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,13 +12,13 @@ int  int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if ( cmp == NULL || size < 1 || array == NULL)
+	if (cmp == NULL || size < 1 || array == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
-		if  (cmp(array[i]))
-			return  (i);
+		if (cmp(array[i]))
+			return (i);
 	}
 	return (-1);
 }
