9665de6dfddb79bfc67f6da24af88e506f3e577f
Your name <you@example.com>
2022-05-12 11:59:00 -0700

    Summation

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+
+/**
+ * sum_them_all - summation of all arguments
+ * @n: number of undefined args
+ * Return: SUM or 0 => Error
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i, sum;
+
+	sum = 0;
+
+	va_start(list, n);
+
+	if(n != 0)
+	{
+		for (i = 0; i < n; sum += va_arg(list, unsigned int), i++);
+	}
+	va_end(list);
+
+	return (sum);
+}
