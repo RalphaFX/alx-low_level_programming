acda594440331fdf8f9596f629ac23075e916ad6
Your name <you@example.com>
2022-05-31 13:06:51 -0700

    Create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * create_file - creates a file 
+ * create_file - creates a file
  * @filename: name of the file to be created
  * @text_content: string to be written into a file
  * with a null terminator
@@ -11,14 +11,15 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int file;
+	int file, i;
 
-	int i = 0;
+	i = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+
 	if (file == -1)
 		return (-1);
 
